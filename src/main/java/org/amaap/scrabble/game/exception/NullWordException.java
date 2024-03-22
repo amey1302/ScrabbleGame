29a9f49505ebf2c32d003e005f6a058628208ef0
amey1302 <ameykulkarni1322@gmail.com>
2024-03-22 22:11:21 +0530

    Add Code for Double or Triple Multiplier letter or word

--- src/main/java/org/amaap/scrabble/game/exception/NullWordException.java
+++
@@ -1,8 +0,0 @@
-package org.amaap.scrabble.game.exception;
-
-public class NullWordException extends InvalidWordException {
-
-    public NullWordException(String message) {
-        super(message);
-    }
-}
