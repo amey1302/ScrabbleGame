07311713a02d3ff2b188877cd083cf2c2d3c34cf
amey1302 <ameykulkarni1322@gmail.com>
2024-04-01 01:09:50 +0530

    Add more test cases for ScoreCaluculator

--- src/test/java/org/amaap/ttp/scrabble/v1/domain/model/ScoreCalculatorTest.java
+++ src/test/java/org/amaap/ttp/scrabble/v1/domain/model/ScoreCalculatorTest.java
@@ -1,9 +1,11 @@
 package org.amaap.ttp.scrabble.v1.domain.model;
 
+import org.amaap.ttp.scrabble.v1.domain.model.exception.EmptyWordException;
 import org.amaap.ttp.scrabble.v1.domain.model.exception.InvalidWordException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ScoreCalculatorTest {
     ScoreCalculator scoreCalculator = new ScoreCalculator();
@@ -19,6 +21,25 @@ class ScoreCalculatorTest {
         int actual = scoreCalculator.getScoreForWord("guardian");
 
         assertEquals(10, actual);
+    }
+
+    @Test
+    void shouldAbleToReturnTheScoreForDoubleOrTripleLetter() throws InvalidWordException {
+        int actual = scoreCalculator.getScoreForWord("guardian");
+
+        assertEquals(10, actual);
+
+    }
+
+    @Test
+    void shouldThrowExceptionForNullWord() {
+        assertThrows(EmptyWordException.class, () -> scoreCalculator.getScoreForWord(null));
+    }
+
+    @Test
+    void shouldAbleToThrowAnExceptionWhenWordIsEmpty() {
+        String word = "";
+        assertThrows(EmptyWordException.class, () -> scoreCalculator.getScoreForWord(word));
     }
 
 }
