c71d33fe40fc0355381fef687bbebf336133a7a7
amey1302 <ameykulkarni1322@gmail.com>
2024-04-01 01:15:16 +0530

    Handled more test cases for ScoreCulator

--- src/test/java/org/amaap/ttp/scrabble/v1/domain/model/ScoreCalculatorTest.java
+++ src/test/java/org/amaap/ttp/scrabble/v1/domain/model/ScoreCalculatorTest.java
@@ -2,6 +2,8 @@ package org.amaap.ttp.scrabble.v1.domain.model;
 
 import org.amaap.ttp.scrabble.v1.domain.model.exception.EmptyWordException;
 import org.amaap.ttp.scrabble.v1.domain.model.exception.InvalidWordException;
+import org.amaap.ttp.scrabble.v1.domain.model.exception.WordContainsDigitsException;
+import org.amaap.ttp.scrabble.v1.domain.model.exception.WordContainsSpecialCharactersException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,35 +13,79 @@ class ScoreCalculatorTest {
     ScoreCalculator scoreCalculator = new ScoreCalculator();
 
     @Test
-    void shouldAbleToReturnTheScoreForCapitalAlphabetWords() throws InvalidWordException {
-        int actual = scoreCalculator.getScoreForWord("GUARDIAN");
-        assertEquals(10, actual);
+    void shouldBeAbleToReturnTheScoreForCapitalAlphabetWords() throws InvalidWordException {
+        // arrange
+        String word = "GUARDIAN";
+        int expected = 10;
 
-    }
-    @Test
-    void shouldAbleToReturnTheScoreForSmallerAlphabet() throws InvalidWordException {
-        int actual = scoreCalculator.getScoreForWord("guardian");
+        // act
+        int actual = scoreCalculator.getScoreForWord(word);
 
-        assertEquals(10, actual);
-    }
-
-    @Test
-    void shouldAbleToReturnTheScoreForDoubleOrTripleLetter() throws InvalidWordException {
-        int actual = scoreCalculator.getScoreForWord("guardian");
-
-        assertEquals(10, actual);
-
+        // assert
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldThrowExceptionForNullWord() {
-        assertThrows(EmptyWordException.class, () -> scoreCalculator.getScoreForWord(null));
+    void shouldBeAbleToReturnTheScoreForSmallerAlphabet() throws InvalidWordException {
+        // arrange
+        String word = "guardian";
+        int expected = 10;
+
+        // act
+        int actual = scoreCalculator.getScoreForWord(word);
+
+        // assert
+        assertEquals(expected, actual);
     }
 
     @Test
-    void shouldAbleToThrowAnExceptionWhenWordIsEmpty() {
-        String word = "";
+    void shouldBeAbleToThrowExceptionForNullWord() {
+        // arrange
+        String word = null;
+
+        // act & assert
         assertThrows(EmptyWordException.class, () -> scoreCalculator.getScoreForWord(word));
     }
 
+    @Test
+    void shouldBeAbleToThrowAnExceptionWhenWordIsEmpty() {
+        // arrange
+        String word = "";
+
+        // act & assert
+        assertThrows(EmptyWordException.class, () -> scoreCalculator.getScoreForWord(word));
+    }
+
+    @Test
+    void shouldBeAbleToThrowAnExceptionWhenWordContainsDigits() {
+        // arrange
+        String word = "34ANT";
+
+        // act & assert
+        assertThrows(WordContainsDigitsException.class, () -> scoreCalculator.getScoreForWord(word));
+    }
+
+    @Test
+    void shouldBeAbleToThrowAnExceptionWhenWordContainsSpecialCharacters() {
+        // arrange
+        String word = "&^%$ANT";
+        String word1 = "&&&&*(";
+
+        // act & assert
+        assertThrows(WordContainsSpecialCharactersException.class, () -> scoreCalculator.getScoreForWord(word));
+        assertThrows(WordContainsSpecialCharactersException.class, () -> scoreCalculator.getScoreForWord(word1));
+    }
+
+    @Test
+    void shouldBeCalculateScoreForWordWithAllLetters() throws InvalidWordException {
+        // arrange
+        String word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int expected = 87;
+
+        // act
+        int actual = scoreCalculator.getScoreForWord(word);
+
+        // assert
+        assertEquals(expected, actual);
+    }
 }
