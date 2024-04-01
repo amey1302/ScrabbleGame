40a26d76397b8820e802c9735c0ff0f03a613454
amey1302 <ameykulkarni1322@gmail.com>
2024-04-01 20:33:36 +0530

    Code Refactor

--- src/test/java/org/amaap/ttp/scrabble/v1/domain/ScrabbleManagerTest.java
+++ src/test/java/org/amaap/ttp/scrabble/v1/domain/ScrabbleManagerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ScoreCalculatorManagerTest {
+public class ScrabbleManagerTest {
     ScrabbleManager scarbbleManager = ScrabbleManager.getInstance();
 
     @Test
